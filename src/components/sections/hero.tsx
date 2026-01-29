e28634d3dd7e0250a5da262f02587839f14c8a47
chetir <chetir147@gmail.com>
2026-01-29 11:28:25 +0000

    по прежнему отоюражается только alt а само изоюражение нет ( на главном

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -14,7 +14,7 @@ export function Hero() {
           src={heroImage.imageUrl}
           alt={heroImage.description}
           fill
-          className="object-cover"
+          className="object-cover -z-10"
           data-ai-hint={heroImage.imageHint}
           priority
         />
